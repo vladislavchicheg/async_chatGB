9cf5d524413b53b39a2ffa36e33097f7fbcdd388
vladislav <vladislavchicheg@gmail.com>
2023-05-29 23:22:55 +0400

    homework5

--- client.py
+++ client.py
@@ -1,13 +1,15 @@
 import argparse
 import json
+import logging
 import sys
 import time
+import log.client_config
 from socket import *
 from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
     RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT, LOGIN
 from common.utils import get_message, send_message
 
-
+CLIENT_LOGGER = logging.getLogger('client')
 def create_presence(login='Guest'):
     message = {
         ACTION: PRESENCE,
@@ -20,6 +22,7 @@ def create_presence(login='Guest'):
 
 
 def process_response(data):
+    CLIENT_LOGGER.debug(f'получено сообщение от сервера: {data}')
     if RESPONSE in data:
         if data[RESPONSE] == 200:
             return '200 : OK'
@@ -38,18 +41,23 @@ def main():
         server_port = DEFAULT_PORT
 
     if server_port < 1024 or server_port > 65535:
+        CLIENT_LOGGER.critical(f'Попытка запуска клиента с недопустимым портом: {server_port}')
         raise ValueError("Указан неверный порт")
+    CLIENT_LOGGER.info(f'запущен клиент с адресом: {server_address}; порт: {server_port}')
 
     s = socket(AF_INET, SOCK_STREAM)
     s.connect((server_address, server_port))
     message_to_server = create_presence()
     send_message(s, message_to_server)
     try:
-        answer = precess_response(get_message(s))
+        answer = process_response(get_message(s))
+        CLIENT_LOGGER.info(f'получен ответ сервера: {answer}')
         print(answer)
-    except (ValueError, json.JSONDecodeError):
+    except json.JSONDecodeError:
+        CLIENT_LOGGER.error('Не удалось декодировать сообщение сервера.')
         print('Не удалось декодировать сообщение сервера.')
-
+    except ConnectionRefusedError:
+        CLIENT_LOGGER.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}')
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,11 +1,13 @@
+import logging
 import sys
 import json
 from socket import *
+import log.server_config
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
     PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, ALERT, LOGIN
 from common.utils import get_message, send_message
 
-
+CLIENT_LOGGER = logging.getLogger('server')
 def process_client_message(message):
     if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
             and USER in message and message[USER][LOGIN] == 'Guest':
@@ -21,6 +23,7 @@ def main():
     if "-p" in sys.argv:
         listen_port = int(sys.argv[sys.argv.index("-p") + 1])
         if listen_port < 1024 or listen_port > 65535:
+            CLIENT_LOGGER.critical(f'Попытка запуска сервера с недопустимым портом: {listen_port}')
             raise ValueError("Неверный номер порта!")
     else:
         listen_port = DEFAULT_PORT
@@ -28,7 +31,7 @@ def main():
         listen_address = sys.argv[sys.argv.index("-a") + 1]
     else:
         listen_address = ""
-
+    CLIENT_LOGGER.info(f'Запущен сервер - порт: {listen_port} ; адрес:{listen_address}')
     s = socket(AF_INET, SOCK_STREAM)
     s.bind((listen_address, listen_port))
     s.listen(MAX_CONNECTIONS)
@@ -37,12 +40,14 @@ def main():
         client, client_address = s.accept()
         try:
             message_from_cient = get_message(client)
+            CLIENT_LOGGER.info(f'Получено сообщение от клиента: {message_from_cient}')
             print(message_from_cient)
-            # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
             response = process_client_message(message_from_cient)
+            CLIENT_LOGGER.debug(f'Отправлено сообщение клиенту: {response}')
             send_message(client, response)
             client.close()
         except (ValueError, json.JSONDecodeError):
+            CLIENT_LOGGER.error(f'Принято некорретное сообщение от клиента:{message_from_cient}')
             print('Принято некорретное сообщение от клиента.')
             client.close()
 
