334330875dafffa6145d17f13dbfc203ce600b3e
vladislav <vladislavchicheg@gmail.com>
2023-06-25 13:24:28 +0400

    homework12

--- common/classes/ClientSender.py
+++ common/classes/ClientSender.py
@@ -3,15 +3,20 @@ import threading
 import time
 
 from common.metaclasses import ClientMaker
-from common.utils import send_message
+from common.utils import send_message, add_contact
 from common.variables import ACTION, EXIT, TIME, ACCOUNT_NAME, MESSAGE, SENDER, DESTINATION, MESSAGE_TEXT
+from error import ServerError
 
+logger = logging.getLogger('client')
+sock_lock = threading.Lock()
+database_lock = threading.Lock()
 
 class ClientSender(threading.Thread, metaclass=ClientMaker):
-    def __init__(self, account_name, sock):
+    def __init__(self, account_name, sock, database):
         self.account_name = account_name
         self.sock = sock
         self.logger = logging.getLogger('client')
+        self.database = database
         super().__init__()
 
     def create_exit_message(self):
@@ -60,7 +65,49 @@ class ClientSender(threading.Thread, metaclass=ClientMaker):
                 print("Команда не распознана, попробойте снова. help - вывести поддерживаемые команды.")
 
     def print_help(self):
-        print("Поддерживаемые команды:")
-        print("message - отправить сообщение. Кому и текст будет запрошены отдельно.")
-        print("help - вывести подсказки по командам")
-        print("exit - выход из программы")
+        print('Поддерживаемые команды:')
+        print('message - отправить сообщение. Кому и текст будет запрошены отдельно.')
+        print('history - история сообщений')
+        print('contacts - список контактов')
+        print('edit - редактирование списка контактов')
+        print('help - вывести подсказки по командам')
+        print('exit - выход из программы')
+
+    def print_history(self):
+        ask = input('Показать входящие сообщения - in, исходящие - out, все - просто Enter: ')
+        with database_lock:
+            if ask == 'in':
+                history_list = self.database.get_history(to_who=self.account_name)
+                for message in history_list:
+                    print(f'\nСообщение от пользователя: {message[0]} от {message[3]}:\n{message[2]}')
+            elif ask == 'out':
+                history_list = self.database.get_history(from_who=self.account_name)
+                for message in history_list:
+                    print(f'\nСообщение пользователю: {message[1]} от {message[3]}:\n{message[2]}')
+            else:
+                history_list = self.database.get_history()
+                for message in history_list:
+                    print(
+                        f'\nСообщение от пользователя: {message[0]}, пользователю {message[1]} от {message[3]}\n{message[2]}')
+
+    # Функция изменеия контактов
+    def edit_contacts(self):
+        ans = input('Для удаления введите del, для добавления add: ')
+        if ans == 'del':
+            edit = input('Введите имя удаляемного контакта: ')
+            with database_lock:
+                if self.database.check_contact(edit):
+                    self.database.del_contact(edit)
+                else:
+                    logger.error('Попытка удаления несуществующего контакта.')
+        elif ans == 'add':
+            # Проверка на возможность такого контакта
+            edit = input('Введите имя создаваемого контакта: ')
+            if self.database.check_user(edit):
+                with database_lock:
+                    self.database.add_contact(edit)
+                with sock_lock:
+                    try:
+                        add_contact(self.sock, self.account_name, edit)
+                    except ServerError:
+                        logger.error('Не удалось отправить информацию на сервер.')
--- common/utils.py
+++ common/utils.py
@@ -8,12 +8,15 @@ import time
 
 from common.decorators import log
 from common.variables import MAX_PACKAGE_LENGTH, ENCODING, DEFAULT_PORT, DEFAULT_IP_ADDRESS, RESPONSE, ERROR, ACTION, \
-    TIME, PRESENCE, USER, ACCOUNT_NAME
+    TIME, PRESENCE, USER, ACCOUNT_NAME, LIST_INFO, REMOVE_CONTACT, USERS_REQUEST, ADD_CONTACT, GET_CONTACTS
+from error import ServerError
 
+client_logger = logging.getLogger('client')
 
 @log
 def get_message(sock):
     encoded_response = sock.recv(MAX_PACKAGE_LENGTH)
+    print(encoded_response)
     if isinstance(encoded_response, bytes):
         json_response = encoded_response.decode(ENCODING)
         response = json.loads(json_response)
@@ -31,10 +34,10 @@ def send_message(sock, message):
 
 
 @log
-def arg_parser_server():
+def arg_parser_server(default_port, default_address):
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", default=DEFAULT_PORT, type=int, nargs="?")
-    parser.add_argument("-a", default="", nargs="?")
+    parser.add_argument("-p", default=default_port, type=int, nargs="?")
+    parser.add_argument("-a", default=default_address, nargs="?")
     namespace = parser.parse_args(sys.argv[1:])
     listen_address = namespace.a
     listen_port = namespace.p
@@ -84,5 +87,94 @@ def arg_parser_client():
         exit(1)
 
     return server_address, server_port, client_name
+
+# Функция запрос контакт листа
+def contacts_list_request(sock, name):
+    client_logger.debug(f'Запрос контакт листа для пользователся {name}')
+    req = {
+        ACTION: GET_CONTACTS,
+        TIME: time.time(),
+        USER: name
+    }
+    client_logger.debug(f'Сформирован запрос {req}')
+    send_message(sock, req)
+    ans = get_message(sock)
+    client_logger.debug(f'Получен ответ {ans}')
+    if RESPONSE in ans and ans[RESPONSE] == 202:
+        return ans[LIST_INFO]
+    else:
+        raise ServerError
+
+
+# Функция добавления пользователя в контакт лист
+def add_contact(sock, username, contact):
+    client_logger.debug(f'Создание контакта {contact}')
+    req = {
+        ACTION: ADD_CONTACT,
+        TIME: time.time(),
+        USER: username,
+        ACCOUNT_NAME: contact
+    }
+    send_message(sock, req)
+    ans = get_message(sock)
+    if RESPONSE in ans and ans[RESPONSE] == 200:
+        pass
+    else:
+        raise ServerError('Ошибка создания контакта')
+    print('Удачное создание контакта.')
+
+
+# Функция запроса списка известных пользователей
+def user_list_request(sock, username):
+    client_logger.debug(f'Запрос списка известных пользователей {username}')
+    req = {
+        ACTION: USERS_REQUEST,
+        TIME: time.time(),
+        ACCOUNT_NAME: username
+    }
+    send_message(sock, req)
+    ans = get_message(sock)
+    if RESPONSE in ans and ans[RESPONSE] == 202:
+        return ans[LIST_INFO]
+    else:
+        raise ServerError('Server Error')
+
+
+# Функция удаления пользователя из контакт листа
+def remove_contact(sock, username, contact):
+    client_logger.debug(f'Создание контакта {contact}')
+    req = {
+        ACTION: REMOVE_CONTACT,
+        TIME: time.time(),
+        USER: username,
+        ACCOUNT_NAME: contact
+    }
+    send_message(sock, req)
+    ans = get_message(sock)
+    if RESPONSE in ans and ans[RESPONSE] == 200:
+        pass
+    else:
+        raise ServerError('Ошибка удаления клиента')
+    print('Удачное удаление')
+
+
+# Функция инициализатор базы данных. Запускается при запуске, загружает данные в базу с сервера.
+def database_load(sock, database, username):
+    # Загружаем список известных пользователей
+    try:
+        users_list = user_list_request(sock, username)
+    except ServerError:
+        client_logger.error('Ошибка запроса списка известных пользователей.')
+    else:
+        database.add_users(users_list)
+
+    # Загружаем список контактов
+    try:
+        contacts_list = contacts_list_request(sock, username)
+    except ServerError:
+        client_logger.error('Ошибка запроса списка контактов.')
+    else:
+        for contact in contacts_list:
+            database.add_contact(contact)
 
 
